4227bdd25daca369b6e19c41d61c85d5b01d0d9c
Alex Verein <tpom6oh@gmail.com>
2019-09-05 10:54:54 +0300

    Example music player app

---
+++ example/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MusicStorage from './MusicStorage';
+import Player from './Player';
+import AvoDebugger from 'avo-react-native-debugger';
+
+export default class App extends Component {
+
+  state = { playing: false, currentTrackIndex: 0, looping: false, time: 0, duration: 0 };
+
+  playerStorage = new MusicStorage();
+  player = new Player();
+
+  componentDidMount() {
+    AvoDebugger.toggleDebugger(true);
+
+    this.player.soundObject.setOnPlaybackStatusUpdate((status) => {
+      this.setState(() => ({ time: status.positionMillis/1000, duration: status.durationMillis/1000 }));
+    })
+    this.player.load(this.playerStorage.trackAsset(this.state.currentTrackIndex));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.label}>{this.playerStorage.trackName(this.state.currentTrackIndex) }</Text>
+
+        <Text style={styles.label}>{this.timingLabel()}</Text>
+
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Button 
+            onPress={this.onPrevTrackPress}
+              title="<"></Button>
+          <Button onPress={this.onPlayPausePress}
+              title={this.playButtonTitle()}></Button>
+          <Button onPress={this.onNextTrackPress}
+              title=">"></Button>
+        </View>
+        <View style={{margin: 16}}>
+          <Button 
+              onPress={this.onLoopPress}
+                title={this.loopButtonTitle()}></Button>
+          </View>
+      </View>
+    );
+  }
+
+  timingLabel = () => {
+    if (isNaN(this.state.time)) {
+      return "";
+    }
+
+    return this.state.time + " / " + this.state.duration;
+  }
+
+  playButtonTitle() {
+    if (this,this.state.playing) {
+      return "Pause";
+    } else {
+      return "Play";
+    }
+  }
+
+  loopButtonTitle() {
+    if (this,this.state.looping) {
+      return "Looping On";
+    } else {
+      return "Looping Off";
+    }
+  }
+
+  onPlayPausePress = () => {
+    if (this.state.playing === false) {
+      this.player.play(this.playerStorage.trackAsset(this.state.currentTrackIndex));
+    } else {
+      this.player.pause();
+    }
+
+    this.setState(() => ({ playing: !this.state.playing }));
+  }
+
+  onNextTrackPress = () => {
+    if (this.playerStorage.hasNext(this.state.currentTrackIndex)) {
+      this.player.stopAndUnload(() => {
+        this.player.load(this.playerStorage.trackAsset(this.state.currentTrackIndex));
+      });
+      this.setState(() => ({ playing: false, currentTrackIndex: this.state.currentTrackIndex + 1 }));
+    }
+  }
+
+  onPrevTrackPress = () => {
+      if (this.playerStorage.hasPrev(this.state.currentTrackIndex)) {
+        this.player.stopAndUnload(() => {
+          this.player.load(this.playerStorage.trackAsset(this.state.currentTrackIndex));
+        });
+        this.setState(() => ({ playing: false, currentTrackIndex: this.state.currentTrackIndex - 1 }));
+      }
+  }
+
+  onLoopPress = () => {
+    this.player.setLooping(!this.state.looping);
+    this.setState(() => ({ looping: !this.state.looping }));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  label: {alignSelf: 'center', margin: 16},
+
+});
---
+++ example/MusicStorage.js
@@ -0,0 +1,32 @@
+export default class MusicStorage {
+
+    tracks = ['greek_loop_mix.mp3', 'jingle_jungle_around_the_bot.wav', 'the_flying_hat.wav'];
+
+    hasNext(fromPosition) {
+        return this.tracks.length - 1 > fromPosition;
+    }
+
+    hasPrev(fromPosition) {
+        return fromPosition > 0;
+    }
+
+    trackName(position) {
+        return this.trackFile(position).split(".")[0];
+    }
+
+    trackFile(position) {
+        return this.tracks[position];
+    }
+
+    trackAsset(position) {
+        switch(position) {
+            case 0:
+                return require('./assets/greek_loop_mix.mp3');
+            case 1:
+                return require('./assets/jingle_jungle_around_the_bot.wav');
+            case 2:
+                return require('./assets/the_flying_hat.wav');
+        }
+       return undefined;
+    }
+}
