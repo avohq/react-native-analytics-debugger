6ff5be26437fa6bbc02ef3121de53b01328d797f
Hugi Hlynsson <hugihlynsson@gmail.com>
2023-12-01 16:16:23 +0000

    WIP: Move react-native to peer-dependencies and update to 0.69

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   preset: "react-native",
-  transform: {
-    "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
-  },
-  transformIgnorePatterns: ["!<rootDir>/node_modules/react-runtime"],
-  testMatch: ["<rootDir>/src/__tests__/**/*.[jt]s?(x)"],
-  testPathIgnorePatterns: ["\\.snap$", "<rootDir>/node_modules/", "testUtils.js"]
+  transformIgnorePatterns: ["node_modules/?!(react-native-root-siblings)"]
 };
